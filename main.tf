58a77642a2ffa0e9434140e0a37d359173094b9f
alexliusq <uilxela7@gmail.com>
2020-06-08 10:51:32 +0800

    hopefully this is a properly configured db

--- main.tf
+++ main.tf
@@ -32,6 +32,14 @@ module "vpc" {
   create_flow_log_cloudwatch_iam_role  = true
 
   tags = var.vpc_tags
+
+  # allow public access to rds
+  create_database_subnet_group           = true
+  create_database_subnet_route_table     = true
+  create_database_internet_gateway_route = true
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 }
 
 data "aws_security_group" "default" {
@@ -76,8 +84,35 @@ module "postgresql_sg" {
   number_of_computed_ingress_with_source_security_group_id = 1
 }
 
-# module "rds" {
-#   source  = "terraform-aws-modules/rds/aws"
-#   version = "2.15.0"
-#   # insert the 11 required variables here
-# }
+module "rds" {
+  source  = "terraform-aws-modules/rds/aws"
+  version = "2.15.0"
+  # insert the 11 required variables here
+
+  identifier = "test-postgres-db"
+  
+  engine = "postgres"
+  engine_version = "12.2"
+  instance_class = "db.t2.micro"
+
+  allocated_storage = 20
+  storage_encrypted = false
+
+  username = "postgres"
+
+  password = "test-password"
+  port = "5432"
+
+  vpc_security_group_ids = [module.postgresql_sg.this_security_group_id]
+
+  maintenance_window = "Mon:00:00-Mon:03:00"
+  backup_window      = "03:00-06:00"
+
+  enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
+
+  subnets_ids = module.vpc.database_subnets
+
+  publicly_accessible = true
+
+  final_snapshot_identifier = "test-postgres-db"
+}
